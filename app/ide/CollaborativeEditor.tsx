91a01f1f4cd22a646e9d3e899c9802922069645f
unknown <apurvjha123@gmail.com>
2025-01-21 16:37:33 +0530

    added cursor

--- app/ide/CollaborativeEditor.tsx
+++ app/ide/CollaborativeEditor.tsx
@@ -4,22 +4,29 @@ import * as Y from "yjs";
 import { LiveblocksYjsProvider } from "@liveblocks/yjs";
 import { useRoom } from "@liveblocks/react/suspense";
 import { useCallback, useEffect, useState } from "react";
+import styles from "./CollaborativeEditor.module.css";
+import { Avatars } from "./Avatars";
 import { Editor } from "@monaco-editor/react";
 import { editor } from "monaco-editor";
 import { MonacoBinding } from "y-monaco";
 import { Awareness } from "y-protocols/awareness";
+import { Cursors } from "./Cursors";
+import { Toolbar } from "./Toolbar";
+
+
 interface EditorComponentProps {
-    content: string;
-    onContentChange: (value: string) => void;
-  }
-// Collaborative text editor with simple rich text, live cursors, and live avatars
-const CollaborativeEditor: React.FC<EditorComponentProps> = ({ content, onContentChange }) => {
-  const [editorRef, setEditorRef] = useState<editor.IStandaloneCodeEditor>();
+  content: string;
+  onContentChange: (value: string) => void;
+}
+// Collaborative code editor with undo/redo, live cursors, and live avatars
+export function CollaborativeEditor({content , onContentChange}:EditorComponentProps) {
   const room = useRoom();
+  const [provider, setProvider] = useState<LiveblocksYjsProvider>();
+  const [editorRef, setEditorRef] = useState<editor.IStandaloneCodeEditor>();
 
   // Set up Liveblocks Yjs provider and attach Monaco editor
   useEffect(() => {
-    let yProvider: any;
+    let yProvider: LiveblocksYjsProvider;
     let yDoc: Y.Doc;
     let binding: MonacoBinding;
 
@@ -27,13 +34,14 @@ const CollaborativeEditor: React.FC<EditorComponentProps> = ({ content, onConten
       yDoc = new Y.Doc();
       const yText = yDoc.getText("monaco");
       yProvider = new LiveblocksYjsProvider(room, yDoc);
+      setProvider(yProvider);
 
       // Attach Yjs to Monaco
       binding = new MonacoBinding(
         yText,
         editorRef.getModel() as editor.ITextModel,
         new Set([editorRef]),
-        yProvider.awareness as Awareness
+        yProvider.awareness as unknown as Awareness
       );
     }
 
@@ -49,15 +57,28 @@ const CollaborativeEditor: React.FC<EditorComponentProps> = ({ content, onConten
   }, []);
 
   return (
-    <Editor
-      onMount={handleOnMount}
-      height="100%"
-      theme="vs-dark"
-      defaultLanguage="typescript"
-      value={content}
-      onChange={(value) => onContentChange(value || '')}
-    />
+    <div className={styles.container}>
+      {provider ? <Cursors yProvider={provider} /> : null}
+      <div className={styles.editorHeader}>
+        <div>{editorRef ? <Toolbar editor={editorRef} /> : null}</div>
+        <Avatars />
+      </div>
+      <div className={styles.editorContainer}>
+        <Editor
+          onMount={handleOnMount}
+          height="100%"
+          width="100hw"
+          theme="vs-dark"
+          defaultLanguage="typescript"
+          defaultValue=""
+          value={content}
+          onChange={(value) => onContentChange(value || '')}
+          options={{
+            tabSize: 2,
+            padding: { top: 20 },
+          }}
+        />
+      </div>
+    </div>
   );
 }
-
-export default CollaborativeEditor;
--- components/globals/socket.js
+++ components/globals/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-const socket = io('http://172.28.176.212:9000')
+const socket = io('http://172.21.214.142:9000')
 
 export default socket
